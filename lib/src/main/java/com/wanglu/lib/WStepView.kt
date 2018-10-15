fe558578c059f6ae6608f35d40ed54bc4e340c43
王璐 <2021765485@qq.com>
2018-10-15 08:36:57 +0800

    first

---
+++ lib/src/main/java/com/wanglu/lib/WStepView.kt
@@ -0,0 +1,15 @@
+package com.wanglu.lib
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class WStepView : View {
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+
+    }
+}
